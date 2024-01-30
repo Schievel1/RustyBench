8c06bec318bc3a941434af68cbca5fea04f03747
Pascal Jäger <pascal.jaeger@leimstift.de>
2024-01-30 09:52:32 +0100

    add log level cli flag
    Signed-off-by: Pascal Jäger <pascal.jaeger@leimstift.de>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use eframe::{egui::ViewportBuilder, run_native};
-use env_logger::Env;
 use rusty_bench::ui::RustyBench;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+}
 
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Parse CLI arguments.
+    let cli = Cli::parse();
+    // Initialize logging.
+    env_logger::Builder::new()
+    .filter_level(cli.verbose.log_level_filter())
+    .init();
+
+    // Run GUI
     let _app: RustyBench = Default::default();
     let options = eframe::NativeOptions {
         viewport: ViewportBuilder::default().with_inner_size([900.0, 350.0]),
         ..Default::default()
     };
     let _ = run_native(
-        "rustyBench",
+        "RustyBench",
         options,
         Box::new(|cc| Box::new(RustyBench::new(cc))),
     );
